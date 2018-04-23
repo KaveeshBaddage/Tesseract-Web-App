61cc48e14c2cd5d6dfa012fa224daff13af93fae
Kaveesha Baddage <MPL_KaveeshaB@millenniumitesp.com>
2018-04-23 22:35:34 +0530

    Add content

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class DashboardPage extends Component {
-    render() {
-        return (
-            <React.Fragment>
-            <div class="row">
-            DashboardPage
-                    </div>
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div >
+          <div className="jumbotron">
+            <h1>Welcome to the Tesseract OCR</h1>
+            <p> Free Online OCR service allows you to convert scanned images to editable text formats and extract text from JPEG/TIFF/BMP files.</p>
+            
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default DashboardPage;
