070fe8ec87e3b3e1b561b7a1bc1e0cf46167f867
Kaveesha Baddage <MPL_KaveeshaB@millenniumitesp.com>
2018-04-23 22:34:36 +0530

    Add HashRouter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import Login from './components/login'
-
+import React, { Component } from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import { connect } from 'react-redux';
+import Dashboard from "./components/Dashboard";
+import Login from "./components/login";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-            <Switch>
-              <Route exact path="/" component={Dashboard}/>
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/logout" component={Login} />
-            </Switch>
+        <HashRouter>
+          <Switch>
+            <Route path="/" component={Dashboard} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/logout" component={Login} />
+          </Switch>
+        </HashRouter>
       </React.Fragment>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+  };
+}
+
+export default connect(mapStateToProps, null)(App);
