5a6aeec863a2db5e9401a626c43c108332f4f528
Kaveesha Baddage <MPL_KaveeshaB@millenniumitesp.com>
2018-04-23 22:35:44 +0530

    Add new component

---
+++ src/components/LangOne.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class LangOne extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div >
+          <div className="jumbotron">
+            <h1>lang one</h1>
+            
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default LangOne;
