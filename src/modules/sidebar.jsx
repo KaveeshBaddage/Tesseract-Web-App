afb215efdbf76530db65c1899653b3627eaeb397
Kaveesha Baddage <MPL_KaveeshaB@millenniumitesp.com>
2018-05-29 22:25:39 +0530

    Change sidebar view

--- src/modules/sidebar.jsx
+++ src/modules/sidebar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { List, Icon, Header } from "semantic-ui-react";
 
 class Sidebar extends React.Component {
   render() {
@@ -8,17 +9,28 @@ class Sidebar extends React.Component {
         <div className="row">
           <div className="column" id="sidebar">
             <div className="ui secondary vertical fluid menu">
-              <Link to={"/dashboard"}>
-                <span className="item">Dashboard</span>{" "}
-              </Link>
-              <Link to={"/getSinhalaText"}>
-                {" "}
-                <span className="item">Get Sinhala Text</span>
-              </Link>
-              <Link to={"/getEnglishText"}>
-                {" "}
-                <span className="item">Get English Text</span>
-              </Link>
+              <Header as="h1" block style={{ width: "95%" }}>
+               Tesseract OCR
+              </Header>
+              <List selection verticalAlign="middle">
+                <List.Item>
+                  <Link to={"/"}>
+                    <span className="item">Dashboard</span>{" "}
+                  </Link>
+                </List.Item>
+                <List.Item>
+                  <Link to={"/getSinhalaText"}>
+                    {" "}
+                    <span className="item">Get Sinhala Text</span>
+                  </Link>
+                </List.Item>
+                <List.Item>
+                  <Link to={"/getEnglishText"}>
+                    {" "}
+                    <span className="item">Get English Text</span>
+                  </Link>
+                </List.Item>
+              </List>
             </div>
           </div>
         </div>
