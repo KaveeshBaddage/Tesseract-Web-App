0dbeecdcfac07a5ae3a4c89984c030326de97188
Kaveesha Baddage <MPL_KaveeshaB@millenniumitesp.com>
2018-05-29 22:24:04 +0530

    Add new content

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
+import { Advertisement,Segment,Header,List,Icon} from "semantic-ui-react";
 
 class DashboardPage extends Component {
   render() {
     return (
       <React.Fragment>
-        <div >
+        <div>
           <div className="jumbotron">
-            <h1>Welcome to the Tesseract OCR</h1>
-            <p> Free Online OCR service allows you to convert scanned images to editable text formats and extract text from JPEG/TIFF/BMP files.</p>
-            
+            <Segment color="grey" style={{ width: "95%"}} textAlign="center">
+              <Header as="h1" block style={{ width: "95%" }}>
+                Welcome to the Tesseract OCR
+              </Header>
+              <p>
+                {" "}
+                Free Online OCR service allows you to convert scanned images to
+                editable text formats and extract text from JPEG/TIFF/BMP files.
+              </p>
+              <br/>
+            </Segment>
+            <Segment stacked style={{ width: "95%" }}>
+            <Header as="h3">
+              <Icon name="question circle outline" />
+              How can you use this service...
+            </Header>
+            <List as="ol">
+            <List.Item as="li"> Click the sidebar according to the desired  language  </List.Item>
+              <List.Item as="li">Upload the file using file uploading section </List.Item>
+              <List.Item as="li">Click <b>Get Text</b> Button </List.Item>
+              <List.Item as="li">Extracted text will be shown a section below file uploadind section  </List.Item>
+            </List>
+          </Segment>
           </div>
         </div>
       </React.Fragment>
