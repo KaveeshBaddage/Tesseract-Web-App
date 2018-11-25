54eb95e567629cc1a047df4ba20890f6309a58d7
kaveeshBaddage <kbaddage98@gmail.com>
2018-11-25 21:17:12 +0530

    Change endpoints

--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -2,5 +2,5 @@ const API = ""
 
 export const LOGIN = API + "login";
 export const LOGOUT = API + "logout";
-export const GET_SINHALA_TEXT = API + "getSinhalaText";
-export const GET_ENGLISH_TEXT = API + "getEnglishText";
+export const GET_EXISTING_DATA_SET = API + "getExistingDataSet";
+export const CREATE_NEW_DATA_SET = API + "createNewDataSet";
